c7cc93bc3f3d3ebd304af8e21ceef43bb4eb0cad
Sigmond Kukla <picoplanetdev@gmail.com>
2023-06-11 14:36:51 -0400

    Update template.py

--- template.py
+++ template.py
@@ -3,7 +3,7 @@ from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
 from spherov2.types import Color
 
-TOY_NAME = "SB-BFEA"
+TOY_NAME = "SB-AAAA"
 
 toy = scanner.find_toy(toy_name=TOY_NAME)
 with SpheroEduAPI(toy) as sphero:
