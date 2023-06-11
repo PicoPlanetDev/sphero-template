bb98a7c07b5f56bc1ae82fbfca449d1f6eded241
PicoPlanetDev <picoplanetdev@gmail.com>
2023-06-11 13:52:47 -0400

    first commit

---
+++ template.py
@@ -0,0 +1,11 @@
+import time
+from spherov2 import scanner
+from spherov2.sphero_edu import SpheroEduAPI
+from spherov2.types import Color
+
+TOY_NAME = "SB-BFEA"
+
+toy = scanner.find_toy(toy_name=TOY_NAME)
+with SpheroEduAPI(toy) as sphero:
+    print("Connected to Sphero ", TOY_NAME)
+    # Begin your code here
